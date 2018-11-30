90da8ac599f74753d50622cf587b115d8a9cfaa7
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-11-30 14:49:31 -0200

    Adiciona parse manual de dinheiro na API 28

--- sample/src/androidTest/java/br/com/concrete/canarinho/sample/Api28FormatterBugTest.java
+++ sample/src/androidTest/java/br/com/concrete/canarinho/sample/Api28FormatterBugTest.java
@@ -15,14 +15,14 @@ public class Api28FormatterBugTest {
     @Test
     @SdkSuppress(minSdkVersion = 28)
     public void moneyFormatSuccessfulRunsOnApi28() {
-        final String value = "1.000,00";
-        assertEquals("1000.00", Formatador.VALOR.desformata(value));
+        final String value = "1.000.000,00";
+        assertEquals("1000000.00", Formatador.VALOR.desformata(value));
     }
 
     @Test
     @SdkSuppress(maxSdkVersion = 27)
     public void moneyFormatSuccessfulRunsOnApi27() {
-        final String value = "1.000,00";
-        assertEquals("1000.00", Formatador.VALOR.desformata(value));
+        final String value = "1.000.000,00";
+        assertEquals("1000000.00", Formatador.VALOR.desformata(value));
     }
 }
